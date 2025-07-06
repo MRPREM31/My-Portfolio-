9f67435d2d60066d58a42b743ea04787fa683b0a
MRPREM31 <mr.prem2006@gmail.com>
2025-07-07 02:23:54 +0530

    Updated last requirments.txt

--- static/js/script.js
+++ static/js/script.js
@@ -1,62 +1,10 @@
-// ========== Smooth Scroll ==========
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        const target = document.querySelector(this.getAttribute('href'));
-        if (target) {
-            e.preventDefault();
-            target.scrollIntoView({ behavior: 'smooth' });
-        }
-    });
-});
-
-// ========== Typewriter Effect ==========
-const textDisplay = document.querySelector('.highlight-role');
-const phrases = ["B.Tech Student", "Frontend Developer", "Flask Learner", "Dream Chaser"];
-let i = 0;
-let j = 0;
-let currentPhrase = [];
-let isDeleting = false;
-let isEnd = false;
-
-function loop() {
-    isEnd = false;
-    textDisplay.innerHTML = currentPhrase.join('');
-
-    if (i < phrases.length) {
-
-        if (!isDeleting && j <= phrases[i].length) {
-            currentPhrase.push(phrases[i][j]);
-            j++;
-            textDisplay.innerHTML = currentPhrase.join('');
-        }
-
-        if (isDeleting && j <= phrases[i].length) {
-            currentPhrase.pop();
-            j--;
-            textDisplay.innerHTML = currentPhrase.join('');
-        }
-
-        if (j == phrases[i].length) {
-            isEnd = true;
-            isDeleting = true;
-        }
-
-        if (isDeleting && j === 0) {
-            currentPhrase = [];
-            isDeleting = false;
-            i++;
-            if (i === phrases.length) {
-                i = 0;
-            }
-        }
-    }
-
-    const speedUp = Math.random() * (80 - 50) + 50;
-    const normalSpeed = Math.random() * (300 - 200) + 200;
-    const time = isEnd ? 1000 : isDeleting ? speedUp : normalSpeed;
-    setTimeout(loop, time);
+const text = "Hello, I’m Prem Prasad Pradhan";
+let index = 0;
+function typeEffect() {
+  if (index < text.length) {
+    document.getElementById("typewriter").innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeEffect, 100);
+  }
 }
-
-if (textDisplay) {
-    loop();
-}
+window.onload = typeEffect;
